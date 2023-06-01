2886a8b427d590c416335a6e82a1e8ea349d59d7
kbtrack94 <128067558+kbtrack94@users.noreply.github.com>
2023-05-31 20:51:42 -0400

    commit

--- app.py
+++ app.py
@@ -1,45 +1,106 @@
-# app.py
+import random
 
-from flask import Flask, jsonify, request
-
-app = Flask(__name__)
-
-workouts = [
-    {
-        'title': 'Cardio Workout',
-        'description': 'A high-intensity cardio routine',
-        'duration': 30,
-        'difficulty': 'Intermediate',
-        'type': 'cardio'
+workout_library = {
+    "Warm Up": {
+        "A": {
+            "Jog": ["Carioca", "Side Skips", "Lunges"],
+            "Drills": "Sprint Drills A & B",
+            "Accelerations": "3 x 20m"
+        },
+        "B": {
+            "Jog": ["Carioca", "Side Skips", "Lunges"],
+            "Drills": "Sprint Drills B & C",
+            "Accelerations": "3 x 40m"
+        },
+        "C": {
+            "Jog": ["Carioca", "Side Skips", "Lunges"],
+            "Drills": "Sprint Drills A & C",
+            "Accelerations": "2 x 40m"
+        },
+        "D": {
+            "Jog": ["Carioca", "Side Skips", "Lunges"],
+            "Drills": "Sprint Drills B & D",
+            "Accelerations": "2 x 60m"
+        },
+        "E": {
+            "Jog": "4x 100m",
+            "Drills": "Hurdles Walkovers 5 x 10h",
+            "Accelerations": "2 x 40m"
+        }
     },
-    {
-        'title': 'Strength Training',
-        'description': 'Full-body strength training session',
-        'duration': 45,
-        'difficulty': 'Advanced',
-        'type': 'strength'
+    "Workout": {
+        "Workout A": {
+            "Distance": "200m",
+            "Pace": "M @ 30s / W @ 34s",
+            "Reps": 6,
+            "Rest": "3m"
+        },
+        "Workout B": {
+            "Distance": "400",
+            "Pace": "M @ 67s / W @ 75s",
+            "Reps": 4,
+            "Rest": "4m"
+        },
+        "Workout C": {
+            "Distance": "150m",
+            "Pace": "M @ 18s / W @ 20s",
+            "Reps": 6,
+            "Rest": "3m"
+        },
+        "Workout D": {
+            "Distance": ["2 x 50m sled pulls", "2 x 30m blocks (3h)", "2 x 40m blocks (4h)"],
+            "Pace": "M @ 18s / W @ 20s",
+            "Reps": 6,
+            "Rest": "3m"
+        }
+    },
+    "Cooldown": {
+        "Cooldown A": {
+            "Exercises": ["Jog 4 x 100m", "Leg Swings x 15", "Exercise 3"]
+        },
+        "Cooldown B": {
+            "Exercises": ["Walk 400m", "Leg Swings x 15"]
+        },
+        "Cooldown C": {
+            "Exercises": ["100m technical build ups", "Side Lunges", "Quad Stretch"]
+        },
+        "Cooldown D": {
+            "Exercises": ["Walk 400 (no shoes)", "Abs x 200"]
+        },
+        "Cooldown E": {
+            "Exercises": ["Hurdle Walkovers 5 x 10h", "Leg Swings x 15"]
+        }
     }
-]
+}
 
-progress = []
+# Define the days of the week
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-@app.route('/api/workouts', methods=['GET'])
-def get_workouts():
-    workout_type = request.args.get('type')
-    filtered_workouts = workouts
-    if workout_type:
-        filtered_workouts = [workout for workout in workouts if workout['type'] == workout_type]
-    return jsonify(filtered_workouts)
-
-@app.route('/api/progress', methods=['GET'])
-def get_progress():
-    return jsonify(progress)
-
-@app.route('/api/progress', methods=['POST'])
-def track_progress():
-    data = request.get_json()
-    progress.append(data)
-    return jsonify(data)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Select a random Warm Up, Workout, and Cooldown for each day of the week
+for day in days_of_week:
+    print(f"Day: {day}")
+    
+    # Randomly select a Warm Up
+    warm_up_key = random.choice(list(workout_library["Warm Up"].keys()))
+    warm_up = workout_library["Warm Up"][warm_up_key]
+    print("Warm Up:")
+    print("Jog:", warm_up["Jog"])
+    print("Drills:", warm_up["Drills"])
+    print("Accelerations:", warm_up["Accelerations"])
+    
+    # Randomly select a Workout
+    workout_key = random.choice(list(workout_library["Workout"].keys()))
+    workout = workout_library["Workout"][workout_key]
+    print("Workout:")
+    print("Distance:", workout["Distance"])
+    print("Pace:", workout["Pace"])
+    print("Reps:", workout["Reps"])
+    print("Rest:", workout["Rest"])
+    
+    # Randomly select a Cooldown
+    cooldown_key = random.choice(list(workout_library["Cooldown"].keys()))
+    cooldown = workout_library["Cooldown"][cooldown_key]
+    print("Cooldown:")
+    print("Exercises:", cooldown["Exercises"])
+    
+    print("----------------------------------")
--- script.js
+++ script.js
@@ -81,4 +81,114 @@ document.addEventListener('DOMContentLoaded', function() {
         progressList.appendChild(progressItem);
       });
     });
+
+  // Randomly select a Warm Up, Workout, and Cooldown for each day of the week
+  const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  const workoutLibrary = {
+    "Warm Up": {
+      "A": {
+        "Jog": ["Carioca", "Side Skips", "Lunges"],
+        "Drills": "Sprint Drills A & B",
+        "Accelerations": "3 x 20m"
+      },
+      "B": {
+        "Jog": ["Carioca", "Side Skips", "Lunges"],
+        "Drills": "Sprint Drills B & C",
+        "Accelerations": "3 x 40m"
+      },
+      "C": {
+        "Jog": ["Carioca", "Side Skips", "Lunges"],
+        "Drills": "Sprint Drills A & C",
+        "Accelerations": "2 x 40m"
+      },
+      "D": {
+        "Jog": ["Carioca", "Side Skips", "Lunges"],
+        "Drills": "Sprint Drills B & D",
+        "Accelerations": "2 x 60m"
+      },
+      "E": {
+        "Jog": "4x 100m",
+        "Drills": "Hurdles Walkovers 5 x 10h",
+        "Accelerations": "2 x 40m"
+      }
+    },
+    "Workout": {
+      "Workout A": {
+        "Distance": "200m",
+        "Pace": "M @ 30s / W @ 34s",
+        "Reps": 6,
+        "Rest": "3m"
+      },
+      "Workout B": {
+        "Distance": "400",
+        "Pace": "M @ 67s / W @ 75s",
+        "Reps": 4,
+        "Rest": "4m"
+      },
+      "Workout C": {
+        "Distance": "150m",
+        "Pace": "M @ 18s / W @ 20s",
+        "Reps": 6,
+        "Rest": "3m"
+      },
+      "Workout D": {
+        "Distance": ["2 x 50m sled pulls", "2 x 30m blocks (3h)", "2 x 40m blocks (4h)"],
+        "Pace": "M @ 18s / W @ 20s",
+        "Reps": 6,
+        "Rest": "3m"
+      }
+    },
+    "Cooldown": {
+      "Cooldown A": {
+        "Exercises": ["Jog 4 x 100m", "Leg Swings x 15", "Exercise 3"]
+      },
+      "Cooldown B": {
+        "Exercises": ["Walk 400m", "Leg Swings x 15"]
+      },
+      "Cooldown C": {
+        "Exercises": ["100m technical build ups", "Side Lunges", "Quad Stretch"]
+      },
+      "Cooldown D": {
+        "Exercises": ["Walk 400 (no shoes)", "Abs x 200"]
+      },
+      "Cooldown E": {
+        "Exercises": ["Hurdle Walkovers 5 x 10h", "Leg Swings x 15"]
+      }
+    }
+  };
+
+  daysOfWeek.forEach(day => {
+    console.log(`Day: ${day}`);
+
+    // Randomly select a Warm Up
+    const warmUpKey = getRandomKey(workoutLibrary["Warm Up"]);
+    const warmUp = workoutLibrary["Warm Up"][warmUpKey];
+    console.log("Warm Up:");
+    console.log("Jog:", warmUp["Jog"]);
+    console.log("Drills:", warmUp["Drills"]);
+    console.log("Accelerations:", warmUp["Accelerations"]);
+
+    // Randomly select a Workout
+    const workoutKey = getRandomKey(workoutLibrary["Workout"]);
+    const workout = workoutLibrary["Workout"][workoutKey];
+    console.log("Workout:");
+    console.log("Distance:", workout["Distance"]);
+    console.log("Pace:", workout["Pace"]);
+    console.log("Reps:", workout["Reps"]);
+    console.log("Rest:", workout["Rest"]);
+
+    // Randomly select a Cooldown
+    const cooldownKey = getRandomKey(workoutLibrary["Cooldown"]);
+    const cooldown = workoutLibrary["Cooldown"][cooldownKey];
+    console.log("Cooldown:");
+    console.log("Exercises:", cooldown["Exercises"]);
+
+    console.log("----------------------------------");
+  });
+
+  // Helper function to get a random key from an object
+  function getRandomKey(obj) {
+    const keys = Object.keys(obj);
+    return keys[Math.floor(Math.random() * keys.length)];
+  }
 });
